bb4e3c8b71c04a7287e3e789163b2bd22c6e52e0
bakyt <bakytkantaeva16@gmail.com>
2024-01-17 14:57:41 +0600

    homework-3

---
+++ homework-3.js
@@ -0,0 +1,65 @@
+"use strict";
+
+//task - 1
+// let user = {};
+
+// user.name = prompt("What's your name?");
+// user.age = prompt("what's your age?");
+// user.gender = prompt("what's your gender?");
+
+// console.log("name:", user.name);
+// console.log("age:", user.age);
+// console.log("gender:", user.gender);
+
+//task -2
+
+// let number = prompt("enter a value:");
+
+// if (typeof +number === "number" && !isNaN(+number)) {
+//   alert("number is: " + number);
+// } else {
+//   alert("it is not a number, try again");
+// }
+
+// task - 3
+
+// let a = +prompt("a?", "");
+
+// switch (a) {
+//   case 0:
+//     alert("0");
+//     break;
+//   case 1:
+//     alert("1");
+//     break;
+//   case 2:
+//   case 3:
+//     alert("2,3");
+//     break;
+//   default:
+//     alert("there is a no this number");
+// }
+
+//task - 4
+
+// let result = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     result += i;
+//   }
+// }
+
+// console.log("the sum of the even numbers:", result);
+
+//task - 5  Перепишите цикл for на while.
+
+// for (let i = 0; i < 3; i++) {
+//   alert(`number ${i}!`);
+// }
+
+// let b = 0;
+// while (b < 3) {
+//   alert(`number ${b}!`);
+//   b++;
+// }
